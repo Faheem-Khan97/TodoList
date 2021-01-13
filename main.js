27fa5791d7b74b8d5e4213b8105e8a39bb970e76
Developer-FK <64476965+Developer-FK@users.noreply.github.com>
2021-01-13 10:23:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,37 +5,61 @@ let containerCheck = document.querySelector('.checked')
 
 
 addBtn.addEventListener('click', () => {
-  
 
-    let todo = document.createElement('input');
+    if(input.value.trim().length >0){
+
+   
+    //Todo elements creation. 1.checkbox 2. input, 3.edit button 4.delete button (one By one)
+
+    //1. checkbox creation
     let check = document.createElement('input');
     check.setAttribute("type", 'checkbox')
-
     check.style.cssText = "margin-right:10px"
-
     check.addEventListener('click', checkClick);
 
-    let editBtn = document.createElement('button');
-    //editBtn.style.cssText = "padding: 5px 12px; background-color: green; color: white;"
 
-    
-    let divItem = document.createElement('div');
-    divItem.classList.add('item');
-    editBtn.classList.add('edit');
-    editBtn.textContent = "Edit"
-    let delBtn = document.createElement('button');
-    delBtn.textContent = "Delete"
-    delBtn.classList.add('remove');
-    console.log(input.value)
+    // 2. Disabled Input
+    let todo = document.createElement('input');
     todo.value = input.value;
     todo.classList.add('item_input');
     todo.setAttribute('disabled', true)
+
+
+    // 3. Edit button 
+    let editBtn = document.createElement('button');
+    editBtn.classList.add('edit');
+    editBtn.textContent = "Edit";
+
+
+    // 4. del button 
+    let delBtn = document.createElement('button');
+    delBtn.textContent = "Delete"
+    delBtn.classList.add('remove');
+
+
+    //creating div tag as a wrapper to all 4 elements
+    let divItem = document.createElement('div');
+    divItem.classList.add('item');
+
+    //add elemets to div
     divItem.append(check,todo, editBtn, delBtn)
+
+    //console.log(input.value)
+    
+    // Appending created div to to container of html
     container.append(divItem);
+
+    //  Removing previous enter value from add input box
     input.value = "";
+
+
+    // adding event listeners to the creted edit and del button.
     editBtn.addEventListener('click', editTodo);
     delBtn.addEventListener('click', delTodo);
-})
+    }
+    input.focus();
+});
+
 
 function editTodo(){
     console.log("inside del todo")
